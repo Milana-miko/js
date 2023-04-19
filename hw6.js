e81c1a5c72681fb70b78a3697676c7987d471754
Milana <milanamiko88@gmail.com>
2023-04-19 15:40:30 +0400

    Homework and lesson 2

--- hw6.js
+++ hw6.js
@@ -1,24 +1,25 @@
 //Task 1
 
-let randomFromZeroToOne = Math.floor(Math.random() * 11);
+
 function checkMoreLess(num) {
+    const randomFromZeroToOne = Math.floor(Math.random() * 11); //0-10 neraryal
     if(randomFromZeroToOne < num) {
-        return "My number is greater than a randomly generated number!";
+        return "My number is greater than a randomly generated number! Generated number is " + randomFromZeroToOne;
     } else if(randomFromZeroToOne > num) {
-        return "Randomly generated number is more than my number!";
-    } else if(randomFromZeroToOne === num) {
-        return "Randomly generated number equals my number!";
+        return "Randomly generated number is " + randomFromZeroToOne + " It's more than my number!";
+    } else {
+        return "Randomly generated number is " + randomFromZeroToOne + " And it's equals my number!";
     }
 }
 
 console.log(checkMoreLess(6));
-console.log(randomFromZeroToOne);
+
 
 //Tas 2
 
-const date = new Date();
-const day = date.getDate();
-const month = date.getMonth();
+const date = new Date(); // 16.4.2023 
+const day = date.getDate(); //16
+const month = date.getMonth(); //4
 
 function checkBirthday(bDay, bMonth) {
     if(bDay === day & bMonth === month) {
@@ -29,6 +30,6 @@ function checkBirthday(bDay, bMonth) {
 }
 
 checkBirthday(16, 3);
-checkBirthday(12, 11);
+checkBirthday(2, 11);
 
 
---
+++ ls2.js
@@ -0,0 +1,6 @@
+function findPow(num1, num2) {
+    for(let num = num1; num <= num2;  num++) {
+        console.log(Math.pow(num, 2));
+    }
+}
+findPow(1, 5);
