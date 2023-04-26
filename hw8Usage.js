a54306f0faf8ed979a865243ca7daeeaf8869939
Milana <70140326+Milana-miko@users.noreply.github.com>
2023-04-26 15:42:03 +0400

    Add files via upload

--- hw8Usage.js
+++ hw8Usage.js
@@ -2,7 +2,9 @@ import convertNumberToWords from "./hw8.js";
 
 import chalk from 'chalk';
 
-//import chalkAnimation from 'chalk-animation';
+import chalkAnimation from 'chalk-animation';
+
+chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
 
 console.log(chalk.bold.italic.green(convertNumberToWords(10)));
 
@@ -10,5 +12,5 @@ console.log(chalk.bgBlue.inverse.yellow(convertNumberToWords(7)));
 
 console.log(chalk.bgBlue.underline(convertNumberToWords(0)));
 
-//console.log(chalkAnimation.rainbow(convertNumberToWords(9)));
+chalkAnimation.rainbow(convertNumberToWords(9));
 
