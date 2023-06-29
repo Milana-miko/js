902523217080ca6185af4b60bb4b1285e4ab005a
Milana <milanamiko88@gmail.com>
2023-06-30 00:51:28 +0400

    Change homework 15

--- hw15.js
+++ hw15.js
@@ -1,36 +1,13 @@
-// 1. Cat breed - maine coon
+// 1. Cat breed - maine coon - "cats?name=maine coon"
+// 2. Jokes - limit = 3 - "jokes?limit=3"
+// 3. Recipe - tiramisu - "recipe?query=tiramisu"
 
-async function catsAsync() {
-    const res = await fetch("https://api.api-ninjas.com/v1/cats?name=maine coon", 
+async function useOppenApiAsync(url) {
+    const res = await fetch("https://api.api-ninjas.com/v1/" + url, 
     {headers: {'X-Api-Key': 'n2kREa3o9YwFZ/vSbZl3ug==d6iO5vkzjKNjMi5o'}});
     return res.json();
 }
 
-catsAsync().then((result) =>{
+useOppenApiAsync("jokes?limit=3").then((result) =>{
    console.log(result); 
-});
-
-
-// 2. Jokes - limit = 3
-
-async function jokesAsync() {
-    const res1 = await fetch("https://api.api-ninjas.com/v1/jokes?limit=3", 
-    {headers: {'X-Api-Key': 'n2kREa3o9YwFZ/vSbZl3ug==d6iO5vkzjKNjMi5o'}});
-    return res1.json();
-}
-
-jokesAsync().then((result1) => {
-    console.log(result1);
-});
-
-// 3. Recipe - tiramisu
-
-async function recipeAsync() {
-    const res2 = await fetch("https://api.api-ninjas.com/v1/recipe?query=tiramisu", 
-    {headers: {'X-Api-Key': 'n2kREa3o9YwFZ/vSbZl3ug==d6iO5vkzjKNjMi5o'}});
-    return res2.json();
-}
-
-recipeAsync().then((result2) => {
-    console.log(result2);
 });
